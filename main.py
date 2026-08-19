562d578c4ff63d3ff51286753c2ee20bb5bd6ca9
LPT94 <lqa.poletto@unesp.br>
2026-08-19 13:40:24 -0300

    Initial commit: project's struct

---
+++ main.py
@@ -0,0 +1,15 @@
+print("""
+
+
+                                -----------------Projeto iniciado!---------------------
+
+      Organização atual:   
+
+                      MaxLanguage(root)
+                           |
+                           |------------Estruturas: Arvore_binaria de indices (estruturas de memória)
+                           |
+                           |-------------Tabelas: Arquivos.txt (dados persistentes [disco])
+                           |
+                           ---------- main.py                    
+""")
